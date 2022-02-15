472f1d35b32f88891d275da972d7abd6c34042a6
Sumit Pachlaniya <97600179+sumitPachlaniya@users.noreply.github.com>
2022-02-15 09:51:57 -0800

    Add files via upload

---
+++ playground.swift
@@ -0,0 +1,9 @@
+struct Book {
+    let name:String
+    let year:String
+    let pages:String
+    }
+    let book = Book(name:"Swift",year:"1999",pages:"450")
+    print(book.name)
+    print(book.year)
+    print(book.pages)
